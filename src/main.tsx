b163692b2fcbe2c2503061815d27a6d1194a2940
Nasfong <fongren007@gmail.com>
2024-03-21 23:35:10 +0700

    11day

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,13 @@ import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import axios from 'axios';
 
 const queryClient = new QueryClient()
+
+axios.defaults.baseURL = "http://localhost:5000"
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,11 +1,12 @@
-import { Box, Button, Modal, TextField, Typography } from '@mui/material';
+import { Box, Button, Modal, TextField } from '@mui/material';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
 
 interface Food {
-  id: number;
+  _id: string;
   name: string;
   image: string
   star: number
@@ -13,12 +14,13 @@ interface Food {
   description: string
 }
 interface FormData {
+  _id: string
   name: string;
   image: string
   star: number
   price: number
   description: string
-  type: string
+  foodType: string
 }
 
 const style = {
@@ -34,98 +36,181 @@ const style = {
 };
 
 const Admin = () => {
-  const { data } = useQuery<Food[]>({
+  const { handleSubmit, register, formState: { errors }, reset } = useForm<FormData>();
+  const { data, refetch } = useQuery<Food[]>({
     queryKey: ['food'],
     queryFn: () =>
-      fetch('http://localhost:5000/food').then((res) =>
-        res.json(),
+      axios.get('/food').then((res) =>
+        res.data,
       ),
   })
   const [open, setOpen] = useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
-  const { handleSubmit, register, formState: { errors } } = useForm<FormData>();
+  const handleOpen = () => {
+    reset({})
+    setOpen(true)
+  }
+  const handleClose = () => {
+    setOpen(false)
+    reset({})
+  }
 
   const mutation = useMutation({
-    mutationFn: (formData: any) => {
-      return fetch('/api', formData)
+    mutationFn: (formData: FormData) => {
+      const formDataToSend = new FormData();
+      formDataToSend.append('name', formData.name);
+      formDataToSend.append('image', formData.image[0]); // Access the first file in the FileList
+      formDataToSend.append('star', formData.star.toString());
+      formDataToSend.append('price', formData.price.toString());
+      formDataToSend.append('description', formData.description);
+      formDataToSend.append('foodType', formData.foodType);
+      return axios.post('/food', formDataToSend, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      }).then(() => {
+        refetch()
+        handleClose()
+        reset()
+      })
     },
   })
-  const onSubmit = (event: any) => {
-    event.preventDefault()
-    mutation.mutate(new FormData(event.target))
+
+
+  const deleteMutation = useMutation({
+    mutationFn: (id: string) => {
+      return axios.delete(`/food/${id}`).then(() => {
+        refetch()
+      })
+    },
+  })
+  const handleDelete = (id: string) => {
+    deleteMutation.mutate(id)
+  }
+
+  const updateMutation = useMutation({
+    mutationFn: (formData: FormData) => {
+      const formDataToSend = new FormData();
+      formDataToSend.append('name', formData.name);
+      // formDataToSend.append('image', null); // Access the first file in the FileList
+      formDataToSend.append('star', formData.star.toString());
+      formDataToSend.append('price', formData.price.toString());
+      formDataToSend.append('description', formData.description);
+      formDataToSend.append('foodType', formData.foodType);
+      formDataToSend.append('_method', 'PUT')
+      return axios.put(`/food/${formData._id}`, formDataToSend, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        }
+      }).then(() => {
+        refetch();
+        handleClose();
+        reset();
+      })
+    },
+  })
+  const handleEdit = (data: any) => {
+    reset({ ...data })
+    handleOpen()
+  }
+
+  const onSubmit = (data: any) => {
+    const image = data.image[0] ? data.image : null
+    if (data._id) updateMutation.mutate({ ...data, image }, data._id)
+    else mutation.mutate(data)
   }
 
   return (
     <div className='mt-52'>
-      <Button variant='contained' onClick={handleOpen}>Add Food</Button>
+      <div className='container'>
+        <div className='text-end'>
+          <Button variant='contained' onClick={handleOpen}>Add Food</Button>
+        </div>
+        <div className="flex flex-wrap">
+          {data?.map((item, index) => (
+            <div key={index} className="w-[250px] border border-gray-200 bg-[#efefef] text-center rounded-xl relative my-5 mx-2">
+              <Link to="/shop/salad/detail" className="w-full h-64 rounded overflow-hidden shadow-lg flex flex-col relative">
+                <img className="w-full h-full object-cover" src={item.image} alt="Sunset in the mountains" />
+                <div className='absolute top-0 right-0 text-white bg-black p-4 bg-opacity-75'>${item.price}</div>
+              </Link>
+              <div className="px-8 flex-grow py-2">
+                <div className="font-bold text-xl mb-2">{item.description}</div>
+                <p className="text-gray-700 text-base">
+                  Lorem ipsum dolor sit amet
+                </p>
+              </div>
+              <Button
+                variant='contained'
+                color='primary'
+                onClick={() => handleEdit(item)}
+              >Edit</Button>
+              <Button
+                variant='contained'
+                color='error'
+                onClick={() => handleDelete(item._id)}
+              >Delete</Button>
+            </div>
+          ))}
+        </div>
+      </div>
       <Modal
         open={open}
         onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
       >
         <form onSubmit={handleSubmit(onSubmit)}>
           <Box sx={style}>
+            <input type="file" {...register('image')} />
             <TextField
               {...register('name', { required: true })}
               label="Name"
               variant="outlined"
               fullWidth
               margin="normal"
-              error={!!errors.name} // Show error if validation fails
+              error={!!errors.name}
               helperText={errors.name ? "First Name is required" : ""}
             />
             <TextField
-              {...register('description')}
+              {...register('description', { required: true })}
               label="Description"
               variant="outlined"
               fullWidth
               margin="normal"
+              error={!!errors.description}
+              helperText={errors.description ? "First Name is required" : ""}
             />
             <TextField
-              {...register('star')}
+              {...register('star', { required: true })}
               label="Star"
               variant="outlined"
               fullWidth
               margin="normal"
+              error={!!errors.star}
+              helperText={errors.star ? "First Name is required" : ""}
             />
             <TextField
-              {...register('price')}
+              {...register('price', { required: true })}
               label="Price"
               variant="outlined"
               fullWidth
               margin="normal"
+              error={!!errors.price}
+              helperText={errors.price ? "First Name is required" : ""}
             />
             <TextField
-              {...register('type')}
+              {...register('foodType', { required: true })}
               label="Type"
               variant="outlined"
               fullWidth
               margin="normal"
+              error={!!errors.foodType}
+              helperText={errors.foodType ? "First Name is required" : ""}
             />
 
-            <Button variant='contained' type='submit'>Create</Button>
+            <Button variant='contained' type='submit'>
+              Create
+            </Button>
           </Box>
         </form>
       </Modal>
-      <div className="flex flex-wrap justify-center">
-        {data?.map((item, index) => (
-          <div key={index} className="w-full max-w-sm border border-gray-200 bg-[#efefef] text-center rounded-xl relative my-5 mx-2">
-            <Link to="/shop/salad/detail" className="w-full h-64 rounded overflow-hidden shadow-lg flex flex-col relative">
-              <img className="w-full h-full object-cover" src={item.image} alt="Sunset in the mountains" />
-              <div className='absolute top-0 right-0 text-white bg-black p-4 bg-opacity-75'>${item.price}</div>
-            </Link>
-            <div className="px-8 flex-grow py-8">
-              <div className="font-bold text-xl mb-2">{item.description}</div>
-              <p className="text-gray-700 text-base">
-                Lorem ipsum dolor sit amet
-              </p>
-              <button className='button2 mt-5 p-2' style={{ boxShadow: "0 0.2rem #e6bb65" }}>Add To Card</button>
-            </div>
-          </div>
-        ))}
-      </div>
     </div>
   )
 }
